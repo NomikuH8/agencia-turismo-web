ff6dac640c103420f3d158a37e89802bea36b093
NomikuH8 <sofaco.checkin@gmail.com>
2023-11-12 17:38:54 -0300

    removido swagger pq achei chato

--- apps/api-fastify/src/routes/v1/clientes/clientes.ts
+++ apps/api-fastify/src/routes/v1/clientes/clientes.ts
@@ -1,5 +1,5 @@
 import { FastifyInstance } from 'fastify'
-import { ClientesController } from '../../controllers/v1/clientes'
+import { ClientesController } from '../../../controllers/v1/clientes'
 
 export default async function clientes(fastify: FastifyInstance): Promise<void> {
   fastify.get('/api/v1/clientes', async (req, res) => {
--- apps/api-fastify/src/utils/getServer.ts
+++ apps/api-fastify/src/utils/getServer.ts
@@ -1,15 +1,11 @@
 import { fastifyAutoload } from '@fastify/autoload'
 import { FastifyInstance, fastify } from 'fastify'
-import { fastifySwagger } from '@fastify/swagger'
 import { fastifyStatic } from '@fastify/static'
 import { fastifyCors } from '@fastify/cors'
 import { join } from 'path'
-import { openApi } from './openapi'
 
 export default async function getServer(): Promise<FastifyInstance> {
-  const server = fastify({
-    logger: true,
-  })
+  const server = fastify()
 
   await server.register(fastifyAutoload, {
     dir: join(__dirname, '..', 'routes'),
@@ -24,10 +20,7 @@ export default async function getServer(): Promise<FastifyInstance> {
     root: join(__dirname, '..', '..', 'public'),
   })
 
-  await server.register(fastifySwagger, {
-    openapi: openApi,
-    hideUntagged: true,
-  })
+  await server.ready()
 
   return await server
 }
