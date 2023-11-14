e0c0a252cef09866298b88804675433ad4ebf172
joao menna <joao.menna@innovo.com.br>
2023-11-14 18:21:10 -0300

    adicionado router de fornecedores

---
+++ apps/api-fastify/src/routes/v1/fornecedores/fornecedores.ts
@@ -0,0 +1,7 @@
+import { FastifyInstance } from "fastify";
+
+export default async function fornecedores(fastify: FastifyInstance) {
+  fastify.get('/api/v1/fornecedores', (req, res) => {
+
+  })
+}
