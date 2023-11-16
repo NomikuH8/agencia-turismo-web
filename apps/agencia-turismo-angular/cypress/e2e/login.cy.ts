2619cd0ed27e151d2169488cda7807cc2290884e
joao menna <joao.menna@innovo.com.br>
2023-11-16 08:21:51 -0300

    corrigido cypress nao funcionar

--- apps/agencia-turismo-angular/cypress/e2e/login.cy.ts
+++ apps/agencia-turismo-angular/cypress/e2e/login.cy.ts
@@ -4,10 +4,17 @@ describe('template spec', () => {
 
     cy.get('input[type=email]').type('abluble@gmail.com')
     cy.get('input[type=password]').type('abluble')
+    cy.get('button').contains('Entrar').click()
 
     cy.contains('Login incorreto!')
 
+    cy.get('input[type=email]').clear()
+    cy.get('input[type=password]').clear()
+
     cy.get('input[type=email]').type('admin@admin.com')
     cy.get('input[type=password]').type('admin')
+    cy.get('button').contains('Entrar').click()
+
+    cy.location().should((loc) => expect(loc.pathname).to.equal('/inicio'))
   })
 })
